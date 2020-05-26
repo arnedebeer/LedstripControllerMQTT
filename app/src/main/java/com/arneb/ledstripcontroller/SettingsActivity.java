306e5c60b791b17e1f991af41b0fc3e2bfe0a59d
Arne de Beer <arnedebeer2001@gmail.com>
2020-05-26 23:45:49 +0200

    [Update] SettingsActivity now properly goes back to MainActivity

--- app/src/main/java/com/arneb/ledstripcontroller/SettingsActivity.java
+++ app/src/main/java/com/arneb/ledstripcontroller/SettingsActivity.java
@@ -3,18 +3,23 @@ package com.arneb.ledstripcontroller;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = this.getClass().getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
@@ -26,16 +31,30 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+//            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     public static class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
         }
 
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            if (key.equals("signature")) {
-                Log.i("", "Preference value was updated to: " + sharedPreferences.getString(key, ""));
+            if (key.equals("mqtt_use_base_topic")){
+                boolean isSelected = sharedPreferences.getBoolean(key, false);
+                getPreferenceScreen().findPreference("mqtt_base_topic").setEnabled(isSelected);
             }
         }
 
